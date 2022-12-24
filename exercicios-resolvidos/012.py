35e200b655b5bae87ae08e8c25223f3cb94fb9ee
daniel-barreto <dbrrt@pm.me>
2022-12-24 18:28:29 +0100

    adicionando novas licoes

---
+++ exercicios-resolvidos/012.py
@@ -0,0 +1,8 @@
+#Some os valores das variáveis num1 e num2, atribuindo o resultado da soma a uma nova variável homônima. Exiba em tela o conteúdo dessa variável.
+
+num1 = 13.45
+num2 = 14.07
+
+sum = num1 + num2
+
+print(sum)
